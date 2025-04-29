d8d46c342ace9887259234b78002dc6e295c7faa
Silvano DAL ZILIO <2209941+dalzilio@users.noreply.github.com>
2025-04-29 09:01:47 +0200

    moved functions from dcsolve to the API

--- cmd/dcsolve/main.go
+++ cmd/dcsolve/main.go
@@ -21,7 +21,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -82,129 +81,10 @@ func main() {
 
 	// We parse the input file and populate our constraint graph. We also keep a
 	// association list between variables names and their index.
-	cg := dcsolver.NewDCS()
-	names := map[string]int{"start": 0}
+	cg, err := dcsolver.ReadSMTLIB(osfile)
 
-	p := &parser{
-		s:     &scanner{r: bufio.NewReader(osfile), pos: &textPos{}},
-		depth: 0,
-		ahead: false,
-	}
-
-LOOP:
-	for {
-		switch tok := p.scan(); tok.tok {
-		case tokEOF:
-			break LOOP
-		case tokLPAR:
-			p.depth++
-		case tokRPAR:
-			p.depth--
-		case tokDECLARE:
-			// (declare-const x Real)
-			if p.depth != 1 {
-				log.Fatalf("Malformed input at %s", tok.pos)
-			}
-			varn := p.scan()
-			if varn.tok != tokVAR {
-				log.Fatalf("Malformed input at %s, awaiting variable declaration, found %s", varn.pos, varn.string)
-			}
-			if tok := p.scan(); tok.tok != tokTYPE {
-				log.Fatalf("Malformed input at %s, missing type declaration Real", tok.pos)
-			}
-			if err := p.scanMultiRPAR(); err != nil {
-				log.Fatalf("Malformed input at %s, missing closing parenthesis", tok.pos)
-			}
-			if *flagv > 0 {
-				fmt.Printf(";; variable \"%s\" added\n", varn.string)
-			}
-			if varn.string == "start" {
-				continue
-			}
-			if _, found := names[varn.string]; found {
-				log.Fatalf("variable %s redefined at %s", varn.string, varn.pos)
-			}
-			names[varn.string] = len(cg.Names)
-			cg.AddVars(varn.string)
-		case tokASSERT:
-			// (assert (op ...))
-			if p.depth != 1 {
-				log.Fatalf("Malformed input at %s", tok.pos)
-			}
-			if tok := p.scan(); tok.tok != tokLPAR {
-				log.Fatalf("Malformed input at %s, missing open parenthesis", tok.pos)
-			}
-			p.depth++
-			tops := p.scan()
-			if tops.tok != tokOP {
-				log.Fatalf("Malformed input at %s, found %s", tops.pos, tops.string)
-			}
-			op := dcsolver.Operation(tops.val)
-			texp := p.scan()
-			switch texp.tok {
-			case tokLPAR:
-				// (assert (op (- y start) 2))
-				p.depth++
-				if tok := p.scan(); tok.tok != tokMINUS {
-					log.Fatalf("Malformed input at %s, missing minus sign", tok.pos)
-				}
-				var1 := p.scan()
-				if var1.tok != tokVAR {
-					log.Fatalf("Awaiting variable at %s, found %s", var1.pos, var1.string)
-				}
-				var2 := p.scan()
-				if var2.tok != tokVAR {
-					log.Fatalf("Awaiting variable at %s, found %s", var2.pos, var2.string)
-				}
-				i1, found1 := names[var1.string]
-				if !found1 {
-					log.Fatalf("variable %s is not defined", var1.string)
-				}
-				i2, found2 := names[var2.string]
-				if !found2 {
-					log.Fatalf("variable %s is not defined", var2.string)
-				}
-				if err := p.scanOneRPAR(); err != nil {
-					log.Fatalf("Malformed input at %s, missing closing parenthesis", tok.pos)
-				}
-				val, err := p.scanValue()
-				if err != nil {
-					log.Fatalf("Malformed input at %s, wrong value", texp.pos)
-				}
-				if *flagv > 0 {
-					fmt.Printf(";; %s - %s %s %d\n", var1.string, var2.string, tops.string, val)
-				}
-				cg.Add(i2, i1, op, val)
-				if !cg.SAT {
-					break LOOP
-				}
-			case tokVAR:
-				// (assert (op x 8))
-				i, found := names[texp.string]
-				if !found {
-					log.Fatalf("variable %s is not defined", texp.string)
-				}
-				val, err := p.scanValue()
-				if err != nil {
-					log.Fatalf("Malformed input at %s, wrong value", texp.pos)
-				}
-				if *flagv > 0 {
-					fmt.Printf(";; %s %s %d\n", texp.string, tops.string, val)
-				}
-				cg.Add(0, i, op, val)
-				if !cg.SAT {
-					break LOOP
-				}
-			default:
-				log.Fatalf("Malformed input at %s, found %s", texp.pos, texp.string)
-			}
-			// parsing: ))
-			if err := p.scanMultiRPAR(); err != nil {
-				log.Fatalf("Malformed input at %s, missing closing parenthesis", tok.pos)
-			}
-		default:
-			log.Fatalf("Malformed input at %s, found %s", tok.pos, tok.string)
-		}
+	if err != nil {
+		log.Fatalln(err)
 	}
 
 	if *flagv > 1 {
@@ -212,18 +92,20 @@ LOOP:
 		fmt.Println()
 	}
 
-	if *flagv > 0 {
-		fmt.Print(cg.PrintFeasible())
-		fmt.Println()
-	}
-
 	if !cg.SAT {
-		fmt.Print("\n*** DCSOLVE: UNSAT ***\n\n")
+		fmt.Print("\n*** DCSOLVE: UNSAT ***\n")
 	} else {
-		fmt.Print("\n*** DCSOLVE: SAT ***\n\n")
+		fmt.Print("\n*** DCSOLVE: SAT ***\n")
 		fmt.Print(cg.PrintFeasible())
 		fmt.Println()
 	}
-
-	callz3(cg)
+	fmt.Println()
+	issat, msg := cg.ExecZ3()
+	if issat {
+		fmt.Print("\n*** Z3: SAT ***\n")
+		fmt.Println(msg)
+		return
+	}
+	fmt.Print("\n*** Z3: UNSAT ***\n")
+	fmt.Println(msg)
 }
